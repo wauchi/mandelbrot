515f08e60cd98ccdbecf4fe4617167fc02424c88
wauchi <nilswalker@bluewin.ch>
2019-06-11 12:13:00 +0200

    Fixed screen size problem
    -Turtle still too slow, slows at a certain point down, need to debug

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,47 +1,48 @@
 from turtle import *
 
-
-height = 360
-width = 360
+HEIGHT, WIDTH = 180, 180
+ITERATION = 10
 screen = Screen()
-screen.screensize(width, height)
-
+screen.setup(WIDTH + 4, HEIGHT + 8)
+screen.screensize(WIDTH, HEIGHT)
+screen.setworldcoordinates(0, 0, WIDTH, HEIGHT)
 
 tu = Turtle()
 tu.hideturtle()
-tu.speed(0)
+tu.speed(100)
 tu.penup()
 
 
-def decreasePoint(n, start1, stop1, start2, stop2):
+def mapPoint(n, start1, stop1, start2, stop2):
     return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2
 
 
-for x in range(width):
-    for y in range(height):
+for x in range(WIDTH):
+    for y in range(HEIGHT):
 
-        a = decreasePoint(x, 0, width, -2, 2)
-        b = decreasePoint(y, 0, height, -2, 2)
-        ca = a
-        cb = b
+        realComponent = mapPoint(x, 0, WIDTH, -2, 2)
+        imaginaryComponent = mapPoint(y, 0, HEIGHT, -2, 2)
+        ca = realComponent
+        cb = imaginaryComponent
 
         n = 0
         z = 0
-        while n < 100:
-            aa = a * a - b * b
-            bb = 2 * a * b
+        while n < ITERATION:
+            aa = realComponent * realComponent - imaginaryComponent * imaginaryComponent
+            bb = 2 * realComponent * imaginaryComponent
 
-            a = aa + ca
-            b = bb + cb
+            realComponent = aa + ca
+            imaginaryComponent = bb + cb
             n += 1
 
-            if abs(a + b) > 16:
+            if abs(realComponent + imaginaryComponent) > 16:
                 break
+
         bright = 'pink'
-        if (n == 100):
+        if n == ITERATION:
             bright = 'black'
 
-        tu.goto(x , y)
+        tu.goto(x, y)
         tu.pendown()
         tu.dot(4, bright)
         tu.penup()
